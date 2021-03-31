5d828d77f5fe2d3c3f9f5d9377f87b48c6913a2c
Manuel Stingl <manuel.stingl@lavita.de>
2021-03-31 13:02:15 +0200

    Extend OlympClient to resolve referenced objects

--- olymp.py
+++ olymp.py
@@ -1,5 +1,12 @@
-from typing import Optional, Tuple
+import logging
+from typing import Dict, Optional, Tuple
+from datetime import datetime, timedelta
+from collections import defaultdict
 from . import Client
+from jose import jwt
+
+
+_logger = logging.getLogger(__name__)
 
 
 class OlympClient(Client):
@@ -46,19 +53,113 @@ class OlympClient(Client):
                 raise self.APIError(self, self._response.text)
 
             if self._response_data:
+                if self.client._referenced_data_auto_load:
+                    self.client.load_referenced_data(self._response_data)
+
                 return self._response_data
 
             return None
 
-    def __init__(self, url, *, timeout=10, verify=True, user: Optional[Tuple[str, str, str]] = None, access_token: Optional[str] = None):
+    def __init__(
+        self,
+        url,
+        *,
+        timeout=10,
+        verify=True,
+        user: Optional[Tuple[str,
+        str,
+        str]] = None,
+        access_token: Optional[str] = None,
+        referenced_data_auto_load: bool = False,
+        referenced_data_clients: Optional[Dict[str, Dict[str, Client]]] = None,
+        referenced_data_expire: timedelta = timedelta(seconds=3600),
+    ):
+        assert user or access_token, "user or access_token must be given"
+
         super().__init__(url, timeout=timeout, verify=verify)
 
         self._user = user
         self._access_token = access_token
-
-        assert user or access_token, "user or access_token must be given"
-
+        self._tenant_id = self._user[2] if self._user else None
+        self._referenced_data_auto_load = referenced_data_auto_load
+        self._ext_clients: defaultdict[str, Dict[str, Client]] = defaultdict(dict)
+        self._ext_clients.update(referenced_data_clients)
+        self._referenced_data_cache = {}
+        self._referenced_data_expire = referenced_data_expire
         self._tokens = {}
+
+    @property
+    def tenant_id(self):
+        if not self._tenant_id:
+            if not self._tokens.get('transaction'):
+                self.auth_transaction()
+
+            claims = jwt.decode(self._tokens['transaction'], options={'verify_signature': False})
+            self._tenant_id = claims.get('ten')
+
+        return self._tenant_id
+
+    def _load_related_data(self, relation: str, tenant_id: str, id: Optional[str] = None, _cache: Optional[dict] = None, **lookup):
+        if _cache is None:
+            _cache = {}
+
+        relation_location = relation.split('/')
+        try:
+            client: Client = self._ext_clients[relation_location[0]][tenant_id]
+
+        except KeyError:
+            _logger.warn("Cannot load data for $rel='%s' with tenant_id='%s'", relation, tenant_id)
+            return
+
+        else:
+            endpoint = client
+            for loc in relation_location[1:]:
+                endpoint = getattr(endpoint, loc)
+
+            cache_key = f'{relation}@{tenant_id}/'
+            if id:
+                cache_key += id
+                if cache_key not in _cache or datetime.utcnow() - _cache[cache_key][1] > self._referenced_data_expire:
+                    _cache[cache_key] = (endpoint[id].get(), datetime.utcnow())
+
+                return _cache[cache_key]
+
+            else:
+                raise NotImplementedError
+
+    def load_referenced_data(self, values: dict, clear_cache: bool = False):
+        """
+        Load referenced data into `values`, performing an in-place update.
+        """
+        if clear_cache:
+            self._referenced_data_cache = {}
+
+        self._ext_clients['olymp'][self.tenant_id] = self
+
+        def enrich_data(values):
+            if isinstance(values, list):
+                for item in values:
+                    enrich_data(item)
+
+            if not isinstance(values, dict):
+                return
+
+            update = None
+            time: datetime = None
+
+            for key, value in values.items():
+                if key == '$rel':
+                    update, time = self._load_related_data(value, **values, _cache=self._referenced_data_cache)
+
+                else:
+                    enrich_data(value)
+
+            if update:
+                enrich_data(update)
+                values.update(update)
+                values['$rel_at'] = time.isoformat()
+
+        enrich_data(values)
 
     def auth_user(self):
         data = self.access.auth.user.post(auth=None, json={
